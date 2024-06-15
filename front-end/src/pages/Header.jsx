4a983338e5b136d3a00e9d391620c317c48f3e16
Gustavo <gustavodossantos1704@gmail.com>
2024-06-15 16:40:20 -0300

    abc

--- front-end/src/pages/Header.jsx
+++ front-end/src/pages/Header.jsx
@@ -1,7 +1,7 @@
-import '../assets/styles/Header.css';
 import { App } from "../layouts/App"
 import { useNavigate } from 'react-router-dom';
 import React from 'react';
+import '../assets/styles/Header.css';
 
 export const Header = () => {
     const navigate = useNavigate();
