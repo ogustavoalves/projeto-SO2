f6a0d4e6252a8726761016263e2598fde62e004b
Gustavo <gustavodossantos1704@gmail.com>
2024-06-15 16:04:56 -0300

    mysql

--- back-end/infra/connection.js
+++ back-end/infra/connection.js
@@ -1,12 +1,11 @@
-const mariadb = require("mariadb");
+const mysql = require("mysql");
 
-const connection = mariadb.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: "3306",
     user: "root",
     password: "12345",
-    database: "projeto_SO2",
-    connectionLimit: 5
+    database: "projeto_SO2"
 });
 
 module.exports = connection;
