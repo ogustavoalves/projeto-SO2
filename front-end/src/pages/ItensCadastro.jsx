e8327535ddab39ca3807a2139333782500e76e6d
Gustavo <gustavodossantos1704@gmail.com>
2024-06-17 15:31:46 -0300

    changing axios ip

--- front-end/src/pages/ItensCadastro.jsx
+++ front-end/src/pages/ItensCadastro.jsx
@@ -6,7 +6,7 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: 'http://18.215.181.114:3000/'
 })
 
 export const  ItensCadastro = () => {
